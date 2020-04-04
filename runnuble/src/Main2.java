6f5fb69980c5d7668ebaa3042871bab5f1f6c53f
Yurii Tkachenko <yurii.tkachenko@ning.com>
2020-04-04 15:53:57 +0300

    Initial commit
    (cherry picked from commit cca8b8f671ed6012b6eaae93e7fad32eb8745b75)

---
+++ runnuble/src/Main2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("a");
+        List<Integer> integers = new ArrayList(strings);
+        integers.add(1);
+        int first = integers.get(0);
+        int second =integers.get(1);
+
+        System.out.println(first);
+        System.out.println(second);
+    }
+
+}
--- src/org/jurassic/Main.java
+++ src/org/jurassic/Main.java
@@ -1,6 +1,7 @@
 package org.jurassic;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Main{
@@ -11,7 +12,7 @@ public class Main{
         Parent parent = new Parent();
         Child child = new Child();
 
-        List<GrandParent> grandParentList = new ArrayList<>();
+        List<GrandParent> grandParentList = new ArrayList<GrandParent>();
         grandParentList.add(grandParent);
         grandParentList.add(parent);
         grandParentList.add(child);
@@ -38,7 +39,104 @@ public class Main{
         childList.get(1).parentMethod();
         childList.get(2).childMethod();
 
-        
+        grandParentList = parentList;
+        grandParentList = childList;
 
+        parentList = grandParentList;
+        parentList = childList;
+
+        childList= grandParentList;
+        childList = parentList;
+
+        List<? extends GrandParent> extendedGrandParentList = new ArrayList<? extends GrandParent>();
+        extendedGrandParentList.add(grandParent);
+        extendedGrandParentList.add(parent);
+        extendedGrandParentList.add(child);
+        extendedGrandParentList.add(new Object());
+        extendedGrandParentList.add(null);
+
+        extendedGrandParentList.get(0).grandParentMethod();
+        extendedGrandParentList.get(1).parentMethod();
+        extendedGrandParentList.get(2).childMethod();
+
+        extendedGrandParentList = grandParentList;
+        extendedGrandParentList = parentList;
+        extendedGrandParentList = childList;
+
+        List<? super Parent> extendedParentList = new ArrayList<? super Parent>();
+        extendedParentList.add(grandParent);
+        extendedParentList.add(parent);
+        extendedParentList.add(child);
+        extendedParentList.add(new Object());
+        extendedParentList.add(null);
+
+        extendedParentList.get(0).grandParentMethod();
+        extendedParentList.get(1).parentMethod();
+        extendedParentList.get(2).childMethod();
+
+        extendedParentList = grandParentList;
+        extendedParentList = parentList;
+        extendedParentList = childList;
+
+        List<?> extendsObjectList = new ArrayList<>();
+
+        extendsObjectList.add(grandParent);
+        extendsObjectList.add(parent);
+        extendsObjectList.add(child);
+        extendsObjectList.add(new Object());
+        extendsObjectList.add(null);
+
+        extendsObjectList.get(0).grandParentMethod();
+        extendsObjectList.get(1).parentMethod();
+        extendsObjectList.get(2).childMethod();
+
+        extendsObjectList = grandParentList;
+        extendsObjectList = parentList;
+        extendsObjectList = childList;
+
+        List objectList = new ArrayList();
+
+        objectList.add(grandParent);
+        objectList.add(parent);
+        objectList.add(child);
+
+        objectList.get(0).grandParentMethod();
+        objectList.get(1).parentMethod();
+        objectList.get(2).childMethod();
     }
+
+    public static void reverseWrong(List<?> list) {
+        List<Object> tmp = new ArrayList<>(list);
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, tmp.get(list.size()-i-1));
+        }
+    }
+
+    public static void reverseRight(List<?> list) {
+        rev(list);
+    }
+
+    private static <T> void rev(List<T> list) {
+        List<T> tmp = new ArrayList<T>(list);
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, tmp.get(list.size()-i-1));
+        }
+    }
+
+    public static <T extends Comparable<T>> T max(Collection<T> coll) {
+        T candidate = coll.iterator().next();
+        for (T elt : coll) {
+            if (candidate.compareTo(elt) < 0) candidate = elt;
+        }
+        return candidate;
+    }
+
+    public static <T extends Object & Comparable<? super T>> T secMax(Collection<? extends T> coll) {
+        T candidate = coll.iterator().next();
+        for (T elt : coll) {
+            if (candidate.compareTo(elt) < 0) candidate = elt;
+        }
+        return candidate;
+    }
+
 }
